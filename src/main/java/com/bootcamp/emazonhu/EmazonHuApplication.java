43f2417362e9b2b27f5e1b66686f8340b11177cc
4ngelLugo <lugomiguel372@gmail.com>
2024-09-04 15:54:46 -0500

    Commit inicial
    -Inicialización de la estructura del proyecto

---
+++ src/main/java/com/bootcamp/emazonhu/EmazonHuApplication.java
@@ -0,0 +1,13 @@
+package com.bootcamp.emazonhu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmazonHuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmazonHuApplication.class, args);
+	}
+
+}
